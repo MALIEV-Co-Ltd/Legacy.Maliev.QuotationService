2f440e730900e6e4ba1ebe6dbc828656dc432a8e
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-08-07 21:12:51 +0700

    Preserve UTC timestamps in quotation migrations

--- Legacy.Maliev.QuotationService.Data/Migrations/Quotation/20260721032128_FixTimestampColumnType.cs
+++ Legacy.Maliev.QuotationService.Data/Migrations/Quotation/20260721032128_FixTimestampColumnType.cs
@@ -1,211 +1,55 @@
-﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Legacy.Maliev.QuotationService.Data.Migrations.Quotation
+namespace Legacy.Maliev.QuotationService.Data.Migrations.Quotation;
+
+/// <inheritdoc />
+public partial class FixTimestampColumnType : Migration
 {
     /// <inheritdoc />
-    public partial class FixTimestampColumnType : Migration
+    protected override void Up(MigrationBuilder migrationBuilder) =>
+        ConvertUtcTimestampColumns(migrationBuilder, toTimestampWithoutTimeZone: true);
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) =>
+        ConvertUtcTimestampColumns(migrationBuilder, toTimestampWithoutTimeZone: false);
+
+    private static void ConvertUtcTimestampColumns(MigrationBuilder migrationBuilder, bool toTimestampWithoutTimeZone)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
+        var targetType = toTimestampWithoutTimeZone
+            ? "timestamp without time zone"
+            : "timestamp with time zone";
+        var defaultSql = toTimestampWithoutTimeZone
+            ? "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'"
+            : "CURRENT_TIMESTAMP";
+
+        foreach (var (table, column, hasDefault) in UtcTimestampColumns)
         {
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ModifiedDate",
-                table: "QuotationHasOrder",
-                type: "timestamp without time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp with time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedDate",
-                table: "QuotationHasOrder",
-                type: "timestamp without time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp with time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ModifiedDate",
-                table: "QuotationFile",
-                type: "timestamp without time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp with time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedDate",
-                table: "QuotationFile",
-                type: "timestamp without time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp with time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ModifiedDate",
-                table: "Quotation",
-                type: "timestamp without time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp with time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ExpirationDate",
-                table: "Quotation",
-                type: "timestamp without time zone",
-                nullable: false,
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp with time zone");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedDate",
-                table: "Quotation",
-                type: "timestamp without time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp with time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ModifiedDate",
-                table: "OrderItem",
-                type: "timestamp without time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp with time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedDate",
-                table: "OrderItem",
-                type: "timestamp without time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp with time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP");
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ModifiedDate",
-                table: "QuotationHasOrder",
-                type: "timestamp with time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedDate",
-                table: "QuotationHasOrder",
-                type: "timestamp with time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ModifiedDate",
-                table: "QuotationFile",
-                type: "timestamp with time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedDate",
-                table: "QuotationFile",
-                type: "timestamp with time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ModifiedDate",
-                table: "Quotation",
-                type: "timestamp with time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ExpirationDate",
-                table: "Quotation",
-                type: "timestamp with time zone",
-                nullable: false,
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedDate",
-                table: "Quotation",
-                type: "timestamp with time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ModifiedDate",
-                table: "OrderItem",
-                type: "timestamp with time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedDate",
-                table: "OrderItem",
-                type: "timestamp with time zone",
-                nullable: true,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone",
-                oldNullable: true,
-                oldDefaultValueSql: "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'");
+            var defaultClause = hasDefault
+                ? $"ALTER TABLE \"{table}\" ALTER COLUMN \"{column}\" SET DEFAULT {defaultSql};"
+                : string.Empty;
+            migrationBuilder.Sql($"""
+                ALTER TABLE "{table}"
+                ALTER COLUMN "{column}" DROP DEFAULT;
+                ALTER TABLE "{table}"
+                ALTER COLUMN "{column}" TYPE {targetType}
+                USING "{column}" AT TIME ZONE 'UTC';
+                {defaultClause}
+                """);
         }
     }
+
+    private static readonly (string Table, string Column, bool HasDefault)[] UtcTimestampColumns =
+    [
+        ("QuotationHasOrder", "ModifiedDate", true),
+        ("QuotationHasOrder", "CreatedDate", true),
+        ("QuotationFile", "ModifiedDate", true),
+        ("QuotationFile", "CreatedDate", true),
+        ("Quotation", "ModifiedDate", true),
+        ("Quotation", "ExpirationDate", false),
+        ("Quotation", "CreatedDate", true),
+        ("OrderItem", "ModifiedDate", true),
+        ("OrderItem", "CreatedDate", true)
+    ];
 }
